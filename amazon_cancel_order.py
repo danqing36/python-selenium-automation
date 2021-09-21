afa57960f990b49db43ef87e8b58d9d76480534e
danqing36 <danqing36@gmail.com>
2021-09-21 11:51:49 -0400

    locator and cancel order

---
+++ amazon_cancel_order.py
@@ -0,0 +1,21 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# init driver
+driver = webdriver.Chrome(executable_path='/Users/fq/Documents/careerist/Automation/python-selenium-automation/chromedriver')
+driver.maximize_window()
+
+driver.get('https://www.amazon.com/gp/help/customer/display.html')
+
+elem = driver.find_element(By.ID,'helpsearch')
+elem.clear()
+elem.send_keys('Cancel Order')
+elem.send_keys(Keys.RETURN)
+
+assert "Cancel Items or Orders" in driver.page_source
+
+driver.close()
+
+
---
+++ locators.py
@@ -0,0 +1,27 @@
+from selenium.webdriver.common.by import By
+
+#find page elements from Amazon sign in
+
+#amazon logo
+driver.find_element(By.XPATH, "//i[@class='a-icon a-icon-logo']")
+
+#continue button
+driver.find_element(By.ID, 'continue')
+
+#need help link
+driver.find_element(By.XPATH, "//a[@href='javascript:void(0)' and @data-action='a-expander-toggle']")
+
+#forgot your password link
+driver.find_element(By.ID, 'auth-fpp-link-bottom')
+
+#other issues with sign in link
+driver.find_element(By.ID, 'ap-other-signin-issues-link')
+
+#create your amazon account button
+driver.find_element(By.ID, 'createAccountSubmit')
+
+#conditions of us link
+driver.find_element(By.XPATH, "//a[text()='Conditions of Use']")
+
+#privacy notice link
+driver.find_element(By.XPATH, "//a[text()='Privacy Notice']")
